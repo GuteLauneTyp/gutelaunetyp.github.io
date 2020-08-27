a3ab87681603582ee832649a8d652759271f2a28
BuckUbel <webquentin@web.de>
2020-08-28 01:58:36 +0200

    Added first production build

---
+++ precache-manifest.86f2fe567a54afb5d1c5ec7047d03e22.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4bccad212252264ec0661f942cde42b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b72c693d9f7c132d5cf0",
+    "url": "/static/css/main.5fe51bcb.chunk.css"
+  },
+  {
+    "revision": "31429bbb02a67b787a3b",
+    "url": "/static/js/2.98f5cf5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.98f5cf5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b72c693d9f7c132d5cf0",
+    "url": "/static/js/main.124836dc.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "1d70eaf5971672bb5a096296b3eb8597",
+    "url": "/static/media/banner1024.1d70eaf5.png"
+  },
+  {
+    "revision": "80e638bab287d0c266846fd95e37be37",
+    "url": "/static/media/bunner_2_1024.80e638ba.png"
+  }
+]);
