ad6355a7aeeeb0bedf810317ad16fe8783105b5c
BuckUbel <webquentin@web.de>
2020-11-09 22:55:50 +0100

    Move dist subtree

---
+++ precache-manifest.1020848dd6ca253ced4a441149d7d7d3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58d1d2d1b276d54710e384391e837862",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7675a62f6497ecf86c8e",
+    "url": "/static/css/main.3311794b.chunk.css"
+  },
+  {
+    "revision": "f243abb54964ad28a58a",
+    "url": "/static/js/2.7ec8d63c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7ec8d63c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7675a62f6497ecf86c8e",
+    "url": "/static/js/main.d6c29a3f.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "1d70eaf5971672bb5a096296b3eb8597",
+    "url": "/static/media/banner1024.1d70eaf5.png"
+  },
+  {
+    "revision": "80e638bab287d0c266846fd95e37be37",
+    "url": "/static/media/bunner_2_1024.80e638ba.png"
+  }
+]);
