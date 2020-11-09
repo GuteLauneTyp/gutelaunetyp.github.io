247d5de20c82d53879c4affa272a054eb8e45882
BuckUbel <webquentin@web.de>
2020-11-09 23:32:05 +0100

    Add missing prod files

---
+++ precache-manifest.81c266243cc4a420e98225f765bafbe8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a84c4311754fef68c9b396b31c525c8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53978db3af0d5e95045b",
+    "url": "/static/css/main.34b1fe08.chunk.css"
+  },
+  {
+    "revision": "66f323e9b9aa367757d1",
+    "url": "/static/js/2.ec4bc4e4.chunk.js"
+  },
+  {
+    "revision": "b13e737c23346402089bf79c557ecf58",
+    "url": "/static/js/2.ec4bc4e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53978db3af0d5e95045b",
+    "url": "/static/js/main.eea4a854.chunk.js"
+  },
+  {
+    "revision": "89b09c3c259a09ffdf4a",
+    "url": "/static/js/runtime-main.74fe0b84.js"
+  },
+  {
+    "revision": "1d70eaf5971672bb5a096296b3eb8597",
+    "url": "/static/media/banner1024.1d70eaf5.png"
+  },
+  {
+    "revision": "80e638bab287d0c266846fd95e37be37",
+    "url": "/static/media/bunner_2_1024.80e638ba.png"
+  }
+]);
