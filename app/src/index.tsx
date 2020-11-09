33eac61adf4d97adba9fb40e708e57f31af8695a
BuckUbel <webquentin@web.de>
2020-11-09 22:40:38 +0100

    Install react-snap

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {hydrate, render} from 'react-dom';
 import './css/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById("root");
+if (rootElement && rootElement.hasChildNodes()) {
+  hydrate(<App/>, rootElement);
+} else {
+  render(<React.StrictMode>
+    <App/>
+  </React.StrictMode>, rootElement);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
